a7548a1c24dab3e3014e0ae71962893aa85d75ac
ringsaturn <ringsaturn.me@gmail.com>
2023-12-05 00:20:52 +0800

    Add benchmark

---
+++ speak_test.go
@@ -0,0 +1,26 @@
+package azuretts_test
+
+import (
+	"testing"
+
+	"github.com/ringsaturn/azuretts"
+)
+
+func BenchmarkToXML(b *testing.B) {
+	speak := azuretts.NewSpeak(
+		azuretts.WithLanguage(azuretts.LanguageZhCN),
+		azuretts.WithVoiceName(azuretts.VoiceNameZhCNYunxiNeural),
+		azuretts.WithStyle(azuretts.StyleChat),
+		azuretts.WithRate(1),
+		azuretts.WithVoiceStyledegree(2),
+		azuretts.WithSpeechText("你好，世界"),
+		azuretts.WithVolume(100),
+	)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := speak.ToXML()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
